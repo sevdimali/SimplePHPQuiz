54a84ab21e1dc7f1728fcaa04fa4e08148a7be84
sevdimali <sevdimaliisayev@mail.ru>
2015-05-15 04:57:53 +0500

    Update db_conn.php

--- includes/db_conn.php
+++ includes/db_conn.php
@@ -8,6 +8,8 @@ DEFINE ('DB_NAME', 'quizdb');
 
 @ $dbc = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
+mysqli_set_charset($dbc,'utf8'); //set the default character set to utf8
+
 if (mysqli_connect_error()){
 	echo "Could not connect to MySql. Please try again";
 	exit();
