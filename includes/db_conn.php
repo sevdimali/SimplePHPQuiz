06adefd89e4373c226abc34a46989b47b47d6cb3
sevdimali <sevdimaliisayev@mail.ru>
2015-05-15 05:09:39 +0500

    Update db_conn.php

--- includes/db_conn.php
+++ includes/db_conn.php
@@ -1,10 +1,10 @@
 <?php 
 
 //Set the database access information as constants
-DEFINE ('DB_USER', 'famous_user');
-DEFINE ('DB_PASSWORD', 'famous_password');
+DEFINE ('DB_USER', 'root');
+DEFINE ('DB_PASSWORD', '');
 DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'quizdb');
+DEFINE ('DB_NAME', 'quiz');
 
 @ $dbc = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
